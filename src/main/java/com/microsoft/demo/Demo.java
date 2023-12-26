d6c338bb7782a96baf9ce26f4c06a6a89402e697
chenhy123 <76434679+chenhy123@users.noreply.github.com>
2023-12-26 11:05:41 +0800

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -2,6 +2,8 @@ package com.microsoft.demo;
 
 public class Demo {
     public void DoSomething(boolean flag){
+        System.out.println("Starting...");
+        
         if(flag){
             System.out.println("I am covered");
             return;
